fbc844aea6f54af38b867558f23d4f171d7519f7
Lucas Massazza <lucasmassazza@gmail.com>
2016-11-17 01:05:54 -0300

    Minor Fixes

--- js/js.js
+++ js/js.js
@@ -10,7 +10,7 @@ var juego={
 		//Tercer elemento
 		iniciar: function(x){
 				juego.instalarpieza(x);
-				juego.mezclarfichas(20);
+				juego.mezclarfichas(1000);
 				juego.capturarteclas();
 		},
 
@@ -153,7 +153,7 @@ var juego={
 			var tempo=30;
 			for (var i=0; i<=veces;i++) {
 				
-				var numeroazar=Math.floor(Math.random()*20);
+				var numeroazar=Math.floor(Math.random()*3);
 
 				switch (numeroazar){
 
